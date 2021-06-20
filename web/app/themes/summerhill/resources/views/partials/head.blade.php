bc6e47bd9056d306b39c9500080584717b349543
mrkaluzny <wojciech.kaluzny@me.com>
2021-06-20 19:20:45 +0200

    Removed jquery ui from header

--- web/app/themes/summerhill/resources/views/partials/head.blade.php
+++ web/app/themes/summerhill/resources/views/partials/head.blade.php
@@ -5,9 +5,4 @@
   @php wp_head() @endphp
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAiN8Z_yXbPgum-N9AmrugwVd962ROQg3E&libraries=&v=weekly">
   </script>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
-
 </head>
